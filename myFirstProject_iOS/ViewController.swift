00b13f4ab9125dd2527a7b1e26c334a3300fa791
Vadym Volodin <vadymvua@gmail.com>
2024-11-18 20:25:23 +0200

    Initial Commit

---
+++ myFirstProject_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myFirstProject_iOS
+//
+//  Created by Vadym Volodin on 18.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
